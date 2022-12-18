7ad5f8156513fb1dd15437529a0779c0cea98fe7
Eldor Azimov <eldorazimov37@gmail.com>
2022-12-18 23:56:46 +0900

    React Router 6

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Routes, Redirect } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import MainHeader from "./components/MainHeader";
@@ -9,21 +9,15 @@ function App() {
     <div>
       <MainHeader />
       <main>
-        <Switch>
+        <Routes>
           <Route path="/" exact>
             <Redirect to="/welcome" />
           </Route>
-          <Route path="/welcome">
-            <Welcome />
-          </Route>
-          <Route path="/products" exact>
-            <Products />
-          </Route>
-          <Route path="/products/:productId">
-            <ProductDetail />
-          </Route>
+          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/products" exact element={<Products />} />
+          <Route path="/products/:productId" element={<ProductDetail />} />
           <Redirect to="/products" />
-        </Switch>
+        </Routes>
       </main>
     </div>
   );
